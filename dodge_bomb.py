cc50387f979fae184ead5edce13018ebe80c4544
kouji <c0c2507261@edu.teu.ac.jp>
2026-04-21 13:37:39 +0900

    練習問題4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -52,6 +52,9 @@ def main():
                 return
         screen.blit(bg_img, [0, 0])
 
+        if kk_rct.colliderect(bb_rct):
+            return
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
